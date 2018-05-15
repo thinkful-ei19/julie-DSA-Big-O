71e389ab9a51ffe013054161d85858429fafde9e
Julie K <julieskim160@gmail.com>
2018-05-15 08:47:59 -0700

    even or odd

---
+++ evenOrOdd.js
@@ -0,0 +1,11 @@
+function isEven(value){
+    if (value % 2 == 0){
+      return true;
+    }
+    else
+      return false;
+  }
+
+
+// Constant Time - O(1) 
+// regardless of size, only get one result 
